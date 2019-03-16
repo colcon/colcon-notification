c991a5f367da992c6a5d95d728ce9fc30d5f75e5
Tim Perkins <code@tprk77.net>
2019-03-16 22:49:05 +0000

    Disable title handler for dumb terminals (#30)
    * Disable title handler for dumb terminals
    
    * add emacs to dictionary of known words
    
    * update doc, remove extra parenthesis

--- colcon_notification/event_handler/terminal_title.py
+++ colcon_notification/event_handler/terminal_title.py
@@ -1,6 +1,7 @@
 # Copyright 2016-2018 Dirk Thomas
 # Licensed under the Apache License, Version 2.0
 
+import os
 import platform
 import sys
 
@@ -14,6 +15,9 @@ from colcon_core.plugin_system import satisfies_version
 class TerminalTitleEventHandler(EventHandlerExtensionPoint):
     """
     Show status in the terminal title.
+
+    The extension is only enabled by default if stdout is a tty-like device
+    and not a dumb terminal.
 
     The extension handles events of the following types:
     - :py:class:`colcon_core.event.job.JobQueued`
@@ -29,6 +33,10 @@ class TerminalTitleEventHandler(EventHandlerExtensionPoint):
         super().__init__()
         satisfies_version(
             EventHandlerExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+        # Setting the title does not work in dumb terminals, i.e., Emacs
+        self.enabled = sys.stdout.isatty() and os.getenv('TERM') != 'dumb'
+
         self._queued_count = 0
         self._ongoing_count = 0
         self._ended_count = 0
